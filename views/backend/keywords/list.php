e4593857774d082cb7904426120f2cd83f614635
surgetcamille <camille.surget@mmibordeaux.com>
2024-02-06 11:36:00 +0100

    motcle (sans create, delete et update)

--- views/backend/keywords/list.php
+++ views/backend/keywords/list.php
@@ -1,3 +1,38 @@
 <?php
 include '../../../header.php'; // contains the header and call to config.php
 
+//Load all keywords
+$MotCle = sql_select("motcle", "*");
+?>
+
+<!-- Bootstrap default layout to display all statuts in foreach -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Mots Clés</h1>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>List des mots clés</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($MotCle as $mot){ ?>
+                        <tr>
+                            <td><?php echo($mot['numMotCle']); ?></td>
+                            <td><?php echo($mot['libMotCle']); ?></td>
+                            <td>
+                                <a href="edit.php?numMotCle=<?php echo($mot['numMotCle']); ?>" class="btn btn-primary">Edit</a>
+                                <a href="delete.php?numMotCle=<?php echo($mot['numMotCle']); ?>" class="btn btn-danger">Delete</a>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+            <a href="create.php" class="btn btn-success">Create</a>
+        </div>
+    </div>
+</div>
+<?php
+include '../../../footer.php'; // contains the footer
