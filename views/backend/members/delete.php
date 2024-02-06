ebc3445a0ac81db463be4f61818b455b7e6e54a8
evemarielouis <eve.tissot@mmibordeaux.com>
2024-02-06 17:26:22 +0200

    member list

--- views/backend/members/delete.php
+++ views/backend/members/delete.php
@@ -18,6 +18,20 @@ if(isset($_GET['numMemb'])){
                     <input id="numMemb" name="numMemb" class="form-control" style="display: none" type="text" value="<?php echo($numMemb); ?>" readonly="readonly" />
                     <input id="prenomMemb" name="prenomMemb" class="form-control" type="text" value="<?php echo($prenomMemb); ?>" readonly="readonly" disabled />
                     <input id="nomMemb" name="nomMemb" class="form-control" type="text" value="<?php echo($nomMemb); ?>" readonly="readonly" disabled />
+                    <input id="nomMemb" name="nomMemb" class="form-control" type="text" value="<?php echo($nomMemb); ?>" readonly="readonly" disabled />
+                    <input id="pseudoMemb" name="pseudoMemb" class="form-control" type="text" value="<?php echo($pseudoMemb); ?>" readonly="readonly" disabled />
+                    <input id="eMailMemb" name="eMailMemb" class="form-control" type="text" value="<?php echo($eMailMemb); ?>" readonly="readonly" disabled />
+                    <input id="dtCreaMemb" name="dtCreaMemb" class="form-control" type="text" value="<?php echo($dtCreaMemb); ?>" readonly="readonly" disabled />
+                    <input id="statut" name="statut" class="form-control" type="text" value="<?php 
+                    if ($numStat !== '1'){
+                        if ($numStat == '2'){
+                            echo 'Modérateur';
+                        }
+                        if ($numStat == '3'){
+                            echo 'Membre';
+                        }
+                    echo($numStat); ?>" readonly="readonly" disabled />
+                
                 </div>
                 <br />
                 <div class="form-group mt-2">
@@ -27,37 +41,7 @@ if(isset($_GET['numMemb'])){
         </div>
     </div>
 </div> 
-<?php } else {
-    ?> 
-    <div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <h1>Suppression Membre</h1>
-        </div>
-        <div class="col-md-12">
-            <!-- Form to delete a thematique -->
-            <form action="<?php echo ROOT_URL . '/api/members/delete.php' ?>" method="post">
-                <div class="form-group">
-                    <label for="numMemb">Nom du Membre</label>
-                    <br>
-                    <select name="numMemb" id="numMemb">
-                        <option value="">Choisir un Membre</option>
-                        <?php 
-                            $result = sql_select('membre');
-                            foreach($result as $req){
-                                echo '<option value="' . $req['numMemb'] . '">' . $req['nomMemb'] .', ' . $req['prenomMemb'] . '</option>';
-                            }
-                        ?>
-                    </select>
-                </div>
-                <br />
-                <div class="form-group mt-2">
-                    <button type="submit" class="btn btn-danger" onclick="return confirm('Est-ce que tu es sûr(e)?')">Confirmer delete ?</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
 
-<?php 
+<?php } else {
+    echo 'Ce membre est administrateur et ne peut pas être supprimé.';
 }
