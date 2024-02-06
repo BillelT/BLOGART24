ff3486fed494fd347caac20863ee1f980a852e69
surgetcamille <camille.surget@mmibordeaux.com>
2024-02-06 12:12:30 +0100

    Update de Keywords

--- views/backend/keywords/edit.php
+++ views/backend/keywords/edit.php
@@ -1,3 +1,31 @@
 <?php
 include '../../../header.php';
 
+if(isset($_GET['numMotCle'])){
+    $numMotCle = $_GET['numMotCle'];
+    $libMotCle = sql_select("motcle", "libMotCle", "numMotCle = $numMotCle")[0]['libMotCle'];
+}
+?>
+
+<!-- Bootstrap form to modify a new keyword -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Modification Mot Clé</h1>
+        </div>
+        <div class="col-md-12">
+            <!-- Form to create a new keyword-->
+            <form action="<?php echo ROOT_URL . '/api/keywords/update.php' ?>" method="post">
+                <div class="form-group">
+                    <label for="libMotCle">Nouveau Mot Clé</label>
+                    <input id="numMotCle" name="numMotCle" class="form-control" style="display: none" type="text" value="<?php echo($numMotCle); ?>" readonly="readonly" />
+                    <input id="libMotCle" name="libMotCle" class="form-control" type="text" value="<?php echo($libMotCle); ?>" />
+                </div>
+                <br />
+                <div class="form-group mt-2">
+                    <button type="submit" class="btn btn-danger">Confirmer update ?</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
