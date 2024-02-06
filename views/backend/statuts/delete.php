b02bcba387a3a9c2d8fa1d35c891dc21cccdc9cb
evemarielouis <eve.tissot@mmibordeaux.com>
2024-02-06 12:34:49 +0200

    test delete

--- views/backend/statuts/delete.php
+++ views/backend/statuts/delete.php
@@ -4,9 +4,7 @@ include '../../../header.php';
 if(isset($_GET['numStat'])){
     $numStat = $_GET['numStat'];
     $libStat = sql_select("STATUT", "libStat", "numStat = $numStat")[0]['libStat'];
-}
-?>
-
+ ?>
 <!-- Bootstrap form to create a new statut -->
 <div class="container">
     <div class="row">
@@ -28,4 +26,37 @@ if(isset($_GET['numStat'])){
             </form>
         </div>
     </div>
-</div>
+</div> 
+<?php } else {
+    ?> 
+    <div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Suppression Statut</h1>
+        </div>
+        <div class="col-md-12">
+            <!-- Form to create a new statut -->
+            <form action="<?php echo ROOT_URL . '/api/statuts/delete.php' ?>" method="post">
+                <div class="form-group">
+                    <label for="numStat">Nom du Statut</label>
+                    <br>
+                    <select name="numStat" id="numStat">
+                        <option value="">Choisir un statut</option>
+                        <?php 
+                            $result = sql_select('statut');
+                            foreach($result as $req){
+                                echo '<option value="' . $req['numStat'] . '">' . $req['libStat'] . '</option>';
+                            }
+                        ?>
+                    </select>
+                </div>
+                <br />
+                <div class="form-group mt-2">
+                    <button type="submit" class="btn btn-danger">Confirmer delete ?</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>';
+<?php 
+}
