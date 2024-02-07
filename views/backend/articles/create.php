48a43e74d48bfbcd0d26d52f48e5decda884b298
surgetcamille <camille.surget@mmibordeaux.com>
2024-02-07 12:36:10 +0100

    article

--- views/backend/articles/create.php
+++ views/backend/articles/create.php
@@ -1,121 +1,60 @@
+
+
 <?php
-include '../../../header.php';
-
-
-//conditions pour l'ajout d'une image
-if (isset($_FILES['image'])) {
-    // Récupère les informations du fichier
-    $tmpName = $_FILES["image"]["tmp_name"];
-    $name = $_FILES["image"]["name"];
-    $size = $_FILES["image"]["size"];
-    $error = $_FILES["image"]["error"];
-
-    echo "Chemin: " . $tmpName . "<br>";
-    echo "Nom : " . $name . "<br>";
-    echo "Taille : " . $size . "<br>";
-
-    if ($error != UPLOAD_ERR_OK) {
-        echo "Erreur lors du téléchargement du fichier.";
-        exit();
-    }
-
-    // Vérifie que la taille du fichier est inférieure à 200 000px.
-    if ($size > 200000) {
-        echo "Téléchargement impossible, fichier trop volumineux";
-        exit();
-    }
-
-    // Récupérer la longueur et la largeur de l'image. Vérifiez que ces deux valeurs ne dépassent pas 80000 pixels.
-    list($width, $height) = getimagesize($tmpName);
-    if ($width <= 80000 && $height <= 80000) {
-        echo "Largeur image : " . $width . "<br>";
-        echo "Hauteur image : " . $height . "<br>";
-
-        $nom_image = time() . "_" . $name;
-
-        // Déplacer le fichier téléchargé vers le dossier approprié
-        $destination = 'images_uploaded/' . $nom_image;
-        move_uploaded_file($tmpName, $destination);
-        echo "L'image a été téléchargée avec succès.";
-
-        // Placer le lien de l'image dans la base de données
-        $sql_requete = "INSERT INTO image (lienImg) VALUES ('$destination');";
-        $request = $bdd->query($sql_requete);
-
-        if ($request) {
-            echo "Image placée dans la base de données.";
-        } else {
-            echo "Erreur lors de l'enregistrement dans la base de données.";
-        }
-    } else {
-        echo "L'image dépasse les 80000 pixels.";
-    }
-}
+include '../../../footer.php';
 ?>
 
-
-
-<!-- Bootstrap form to create a new article -->
+<?php
+include '../../../header.php';
+?>
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <h1>Création nouvel article</h1>
+            <h1>Création nouveau Article</h1>
         </div>
         <div class="col-md-12">
-            <!-- Form to create a new member -->
+            <!-- Form to create a new statut -->
             <form action="<?php echo ROOT_URL . '/api/articles/create.php' ?>" method="post">
                 <div class="form-group">
-                    <!-- titre -->
-                    <label for="libTitrArt">Titre de l'article</label>
-                    <input id="libTitrArt" name="titreArticle" class="form-control" type="text" autofocus="autofocus" placeholder="100 caractères max" maxlength="100" />
-                    <p><br></p>
-                    <!-- date création -->
-                    <label for="dtCreaArt">Date Création</label>
-                    <input type="date" id="dtCreaArt" name="dateArticle">
-                    <input type="time" id="dtCreaArt" name= "heureArticle"/>
-                    <p><br></p>
-                    <!-- chapo -->
-                    <label for="libChapoArt">Chapeau</label>
-                    <textarea id="libChapoArt" name="Chapo" class="form-control" type="text" autofocus="autofocus" placeholder="Décrivez le chapeau sur 500 caractères" maxlength="500" rows="10" ></textarea>
-                    <p><br></p>
-                    <!-- accroche -->
-                    <label for="libAccrochArt">Accroche</label>
-                    <textarea id="libAccrochArt" name="Accroche" class="form-control" type="text" autofocus="autofocus" placeholder="Accroche Paragraphe 1. Sur 100 caractères" maxlength="100" rows="5" ></textarea>
-                    <p><br></p>
-                    <!-- paragraphe 1 -->
-                    <label for="parag1Art">Paragraphe 1</label>
-                    <textarea id="parag1Art" name="Paragraphe1" class="form-control" type="text" autofocus="autofocus" placeholder="Décrivez le premier paragraphe sur 1200 caractères." maxlength="1200" rows="20"></textarea>
-                    <p><br></p>
-                    <!-- sous-titre 1 -->
-                    <label for="libSsTitr1Art">Sous Titre paragraphe 2</label>
-                    <input id="libSsTitr1Art" name="SousTitre1" class="form-control" type="text" autofocus="autofocus" placeholder="Sous titre du paragraphe 2 sur 100 caractères." maxlength="100" />
-                    <p><br></p>
-                    <!-- paragraphe 2 -->
-                    <label for="parag2Art">Paragraphe 2</label>
-                    <textarea id="parag2Art" name="Paragraphe2" class="form-control" type="text" autofocus="autofocus" placeholder="Décrivez le second paragraphe sur 1200 caractères." maxlength="1200" rows="20"></textarea>
-                    <p><br></p>
-                    <!-- sous-titre 2 -->
-                    <label for="libSsTitr2Art">Sous Titre paragraphe 3</label>
-                    <input id="libSsTitr2Art" name="SousTitre2" class="form-control" type="text" autofocus="autofocus" placeholder="Sous titre du paragraphe 3 sur 100 caractères." maxlength="100" />
-                    <p><br></p>
-                    <!-- paragraphe 3 -->
-                    <label for="parag3Art">Paragraphe 3</label>
-                    <textarea id="parag3Art" name="Paragraphe2" class="form-control" type="text" autofocus="autofocus" placeholder="Décrivez le troisième paragraphe sur 1200 caractères." maxlength="1200" rows="20"></textarea>
-                    <p><br></p>
-                    <!--  conclusion -->
-                    <label for="libConclArt">Conclusion</label>
-                    <textarea id="libConclArt" name="Conclusion" class="form-control" type="text" autofocus="autofocus" placeholder="Ecrivez la conclusion sur 800 caractères." maxlength="800" rows="15"></textarea>
-                    <p><br></p>
-                    <!-- ajout d'une image !-->
-                    <form action="<?php echo ROOT_URL . '/api/articles/create.php' ?>" method="post" enctype="multipart/form-data">
-                        <p>Importez une image</p>
-                        <input type="file" accept=".jpg, .jpeg, .png, .gif" name="image" />
-                        <p>>> Extension des images acceptées : .jpg, .gif, .png, .jpeg <br>(largeur, hauteur, taille max : 80000px, 80000px, 200 000 Go)</p>
-                        <input type="submit" value="Enregistrer">
-                        <p><br></p>
-                    </form>
-                    <form action="<?php echo ROOT_URL . '/api/articles/create.php' ?>" method="post">
+                    <label for="libStat">Titre</label>
+                    <input id="titre" name="libTitrArt" class="form-control" type="text" autofocus="autofocus" placeholder="Sur 100 car.">
+                    <br>
+                    <label for="libStat">Date création</label>
+                    <input id="Datecreation" name="dtCreaArt" class="form-control" type="datetime-local" autofocus="autofocus" placeholder="jj/mm/aaaa">
+                    <br>
+                    <label for="libStat">Chapeau</label>
+                    <textarea id="Chapeau" name="libChapoArt" class="form-control" placeholder="Décrivez le chapeau. Sur 500 caractères." maxlength="500" style="height: 200px;"></textarea>
+                    <br>
+                    <label for="libStat">Accroche paragraphe 1</label>
+                    <input id="Accroche1" name="libAccrochArt" class="form-control" type="text" autofocus="autofocus" maxlength="100" placeholder="">
+                    <br>sur 100 car.
+                    <label for="libStat">Paragraphe 1</label>
+                    <textarea id="Paragraphe1" name="parag1Art" class="form-control" placeholder="Décrivez le premier paragraphe. Sur 1200 car." maxlength="1200" style="height: 200px;"></textarea>
+                    <br>
+                    <label for="libStat">Sous-titre 1</label>
+                    <input id="Soustitre1" name="libSsTitr1Art" class="form-control" type="text" autofocus="autofocus" maxlength="100" placeholder="Sur 100 car.">
+                    <br>
+                    <label for="libStat">Paragraphe 2</label>
+                    <textarea id="Paragraphe2" name="parag2Art" class="form-control" placeholder="Décrivez le deuxième paragraphe. Sur 1200 car." maxlength="1200" style="height: 200px;"></textarea>
+                    <br>
+                    <label for="libStat">Sous-titre 2</label>
+                    <input id="Soustitre2" name="libSsTitr2Art" class="form-control" type="text" autofocus="autofocus" maxlength="100" placeholder="Sur 100 car.">
+                    <br>
+                    <label for="libStat">Paragraphe 3</label>
+                    <textarea id="Paragraphe3" name="parag3Art" class="form-control" placeholder="Décrivez le troisième paragraphe. Sur 1200 car." maxlength="1200" style="height: 200px;"></textarea>
+                    <br>
+                    <label for="libStat">Conclusion</label>
+                    <textarea id="Conclusion" name="libConclArt" class="form-control" placeholder="Décrivez la conclusion. Sur 800 car." maxlength="500" style="height: 200px;"></textarea>
+                    <br>
+                   
+
+                    <!-- image !-->
+                    <label for="imageInput">Choisir une image :</label>
+                    <input type="file" id="imageInput" name="urlPhotArt" accept=".jpg, .jpeg, .png, .gif" maxlength="80000" width="80000" height="80000" size="200000000000">
+                    
+                    <p>>> Extension des images acceptées : .jpg, .gif, .png, .jpeg (lageur, hauteur, taille max : 80000px, 80000px, 200 000 Go)</p>
                     <!-- choix de la thématique !-->
+                    <p><br></p>
                     <label for="libThem">Thématique :<br></label>
                     <select name="thematique" id="libThem">
                     <option value="">Cliquez et selectionnez une thématique</option>
@@ -126,66 +65,39 @@ if (isset($_FILES['image'])) {
                             }
                         ?>
                     </select>
-                    <p><br></p>
-                    </form>
-                    <!-- selection de mots clés dans la liste !--> 
-                    <form action="<?php echo ROOT_URL . '/api/articles/create.php' ?>" method="post">
-                    <form action="<?php echo ROOT_URL . '/api/articles/create.php' ?>" method="post">
-                        <!-- Liste déroulante pour les mots-clés disponibles -->
-                        <label for="libMotCle">Mots Clés Disponibles:</label>
-                        <p><br></p>
-                        <select name="motcle" id="libMotCle" multiple>
-                            <!-- Options de mots-clés ici -->
-                            <?php 
-                                $result = sql_select('motcle');
-                                foreach($result as $req){
-                                    echo '<option value="' . $req['libMotCle'] . '">' . $req['libMotCle'] . '</option>';
-                                }
-                            ?>
-                        </select>
-                        <button type="button" onclick="addKeyword()">Ajouter</button>
-                        <p><br></p>
-                        <!-- Liste déroulante pour les mots-clés sélectionnés -->
-                        <label for="selectMotCle">Mots Clés Sélectionnés:<br></label>
-                        <p><br></p>
-                        <select name="selectMotCle[]" id="selectMotCle" multiple>
-                            <!-- Options de mots-clés sélectionnés ici -->
-                        </select>
-                        <button type="button" onclick="removeKeyword()">Supprimer</button>
-                    </form>
-                    <p><br></p>
-
-<!-- Script JavaScript pour manipuler les sélections -->
-<script>
-    function moveSelectedOptions(fromSelect, toSelect) {
-        var selectedOptions = Array.from(fromSelect.selectedOptions);
-
-        selectedOptions.forEach(function(option) {
-            toSelect.add(new Option(option.text, option.value));
-            option.remove();
-        });
-    }
-
-    function addKeyword() {
-        moveSelectedOptions(document.getElementById('libMotCle'), document.getElementById('selectMotCle'));
-    }
-
-    function removeKeyword() {
-        moveSelectedOptions(document.getElementById('selectMotCle'), document.getElementById('libMotCle'));
-    }
-</script>
+                   
 
 
-
-                    </div>
-                <br/>
+                </div>
+                <br>
                 <div class="form-group mt-2">
+
                     <button type="submit" class="btn btn-primary">Confirmer create ?</button>
                 </div>
+                <?php 
+                    $requiredFields = ['libTitrArt', 'dtCreaArt', 'libChapoArt', 'libAccrochArt', 'parag1Art', 'libSsTitr1Art', 'parag2Art', 'libSsTitr2Art', 'parag3Art', 'libConclArt',];
+
+                    foreach ($requiredFields as $field) {
+                        if (empty($_POST[$field])) {
+                            echo "Veuillez remplir tous les champs du formulaire.";
+                            exit();
+                        }
+                    }
+                    ?>
+                
+                
+
+
             </form>
         </div>
     </div>
 </div>
 
+
+
+
 <?php
-include '../../../footer.php';
+include '../../../header.php';
+
+
+?>
