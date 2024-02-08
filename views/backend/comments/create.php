e3f66a16cd4edcaf1d181e5ed90c643c2934f47d
surgetcamille <camille.surget@mmibordeaux.com>
2024-02-08 19:23:11 +0100

    Comment

--- views/backend/comments/create.php
+++ views/backend/comments/create.php
@@ -1,10 +1,12 @@
 <?php
-include '../../../header.php';
+require_once '../../../header.php';
+
 
 // if (!isset($_SESSION['numStat']) || $_SESSION['numStat'] == 1 && $_SESSION['numStat'] !== 2 && $_SESSION['numStat'] !== 2 ){
     //header('Location: ../../index.php');
    // exit();
 //}
+
 
 
 
