10696597c3e8fada3757b54f867a0a13310e8a73
surgetcamille <camille.surget@mmibordeaux.com>
2024-02-08 18:55:21 +0100

    commentaires

--- views/backend/comments/create.php
+++ views/backend/comments/create.php
@@ -1,37 +1,48 @@
 <?php
 include '../../../header.php';
 
-if (!isset($_SESSION['numStat']) || $_SESSION['numStat'] == 1 && $_SESSION['numStat'] !== 2 && $_SESSION['numStat'] !== 2 ){
-    header('Location: ../../index.php');
-    exit();
-}
+// if (!isset($_SESSION['numStat']) || $_SESSION['numStat'] == 1 && $_SESSION['numStat'] !== 2 && $_SESSION['numStat'] !== 2 ){
+    //header('Location: ../../index.php');
+   // exit();
+//}
 
-sql_select('article','*');
+
 
 ?>
 
-<?php
-if ($_POST['submit']){
-    if(isset($_POST['pseudo'], $_POST['commentaire'])&& !empty($_POST['pseudo'])&& !empty($_POST['commentaire'])) {
-        header('Location : /views/backend/comments/create.php');
-    }else{
-        $c_erreur = "Tous les champs doivent être complétés";
-    }
-}
-?>
 
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <h1>Création nouveau Article</h1>
+            <h1>Commentaire</h1>
         </div>
         <div class="col-md-12">
-            <!-- Form to create a new comment -->
-            <form action="<?php echo ROOT_URL . '/api/comments/create.php' ?>" method="post" id="form">
-            <input type="text" name="pseudo" placeholder="Votre pseudo"/><br>
-            <textarea name="commentaire" placeholder="Votre Commentaire..."></textarea>
-            <br>
-            <input type="submit" value="poster" name="submit"/>
+                
+        <form action="/api/comments/create.php" method="post">
+            <label for="pseudoMemb">Pseudo :</label>
+            <input type="text" name="pseudoMemb" required>
 
+            <label for="password">Mot de passe :</label>
+            <input type="password" name="password" required>
+
+            <label for="article">Sélectionner l'article :</label>
+            <select name="article" required>
+                <option value="1">Article Evenement</option>
+                <option value="2">Article Portrait</option>
+            </select>
+
+            <label for="comment">Commentaire :</label>
+            <textarea name="comment" required></textarea>
+
+            <input type="submit" value="Poster le commentaire">
         </form>
-        <?php if(isset($c_erreur)){echo "$c_erreur";}
+
+
+</div>
+</div>    
+</div>
+        
+?>
+<?php
+include '../../../footer.php';
+?>
