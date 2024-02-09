375e582566415f78dfdbc7dab3184e38b9ec47a0
evemarielouis <eve.tissot@mmibordeaux.com>
2024-02-09 12:19:52 +0200

    thematique check bug

--- api/thematiques/delete.php
+++ api/thematiques/delete.php
@@ -3,6 +3,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 require_once '../../functions/ctrlSaisies.php';
 
 $numThem = ctrlSaisies($_POST['numThem']);
+$libThem = ctrlSaisies($_POST['libThem']);
 
 sql_delete('THEMATIQUE', "numThem = $numThem");
 
