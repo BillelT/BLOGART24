d0b3c8369b8f78e2fd1aa2ed9e98bca3ebbe482c
evemarielouis <eve.tissot@mmibordeaux.com>
2024-02-09 21:07:01 +0200

    index

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
     require_once 'header.php';
-    $article1 = sql_select('ARTICLE', '*', "numArt = '1'");
+    $article1 = sql_select('ARTICLE', '*', "numArt = '1'")[0];
     $libTitrArt1 = $article1['libTitrArt'];
-    $libChapoArt1 = $article1['libTitrArt'];
-    $article2 = sql_select('ARTICLE', '*', "numArt = '1'");
+    $libChapoArt1 = $article1['libChapoArt'];
+    $article2 = sql_select('ARTICLE', '*', "numArt = '2'")[0];
     $libTitrArt2 = $article2['libTitrArt'];
-    $libChapoArt2 = $article2['libTitrArt'];
+    $libChapoArt2 = $article2['libChapoArt'];
     
 ?>
 <!DOCTYPE html>
