44c8b8622a444edb44801e245ed54dcfbbc32504
evemarielouis <eve.tissot@mmibordeaux.com>
2024-02-09 12:09:32 +0200

    modif articles

--- api/articles/update.php
+++ api/articles/update.php
@@ -3,7 +3,6 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 require_once '../../functions/ctrlSaisies.php';
 
-
 $dtCreaArt = ctrlSaisies($_POST['dtCreaArt']);
 $dtMajArt = date("dtMajArt");
 $libTitrArt = ctrlSaisies($_POST['libTitrArt']);
@@ -18,24 +17,11 @@ $libSsTitr2Art = ctrlSaisies($_POST['libSsTitr2Art']);
 $parag3Art = ctrlSaisies($_POST['parag3Art']);
 $parag3Art = BBCode($parag3Art);
 $libConclArt = ctrlSaisies($_POST['libConclArt']);
-$urlPhotArt = ctrlSaisies($_FILES['urlPhotArt']);
+$urlPhotArt = ctrlSaisies($_POST['urlPhotArt']);
 $numMotCle = $_POST['motCle'];
 $numArt = ctrlSaisies($_POST['numArt']);
-
-
-// VARIABLES POUR FONCTION UPDATE
-$set_art = "dtMajArt = '$dtMajArt',
-libTitrArt = '$libTitrArt',
-libChapoArt = '$libTitrArt', 
-libAccrochArt = '$libAccrochArt',
-parag1Art = '$parag1Art', 
-libSsTitr1Art = '$libSsTitr1Art',
-parag3Art = '$parag3Art',
-libConclArt = '$libConclArt', 
-urlPhotArt = '$urlPhotArt', 
-numThem = '$numThem'";
-$where_num = "numArt = '$numArt'";
-$table_art = "ARTICLE";
+$numThem = ctrlSaisies($_POST['numThem']);
+$libThem = sql_select('THEMATIQUE', 'libThem', "numThem = '$numThem'");
 
 // VERIFIER FIELDS
 
@@ -50,40 +36,30 @@ $requiredFields = ['libTitrArt', 'dtCreaArt', 'libChapoArt', 'libAccrochArt', 'p
 
 // IMAGES TEST
 
-if(isset($_FILES['urlPhotArt'])){
-    var_dump($_FILES);
-    $files = $_FILES['urlPhotArt'];
-    $tmpName = $files['tmp_name'];
-    $name = $files['name'];
-    $size = $files['size'];
-    $error = $files['error'];
-    
-    if(!$error == 0){ // IF THERES AN ERROR
-        // VERIFY FILE SIZE
-        if ($size > 10000000) {
-        echo "Le fichier est trop volumineux.";
-        exit();
-        }
+$nom_image = time() . '_' . $urlPhotArt;
+move_uploaded_file($urlPhotArt, 'src/uploads' . $nom_image);
 
-        // VERIFY IMG SIZE
-        list($width, $height) = getimagesize($tmpName);
-        if ($width > 5000 || $height > 5000) {
-        echo "L'image est trop grande.";
-        exit();
-        }
 
-    } else { // IF THERES NO ERROR
-        $nom_image = time() . '_' . $urlPhotArt;
-        move_uploaded_file($tmpName, 'src/uploads' . $nom_image);
-    }
-}
+// VARIABLES POUR FONCTION UPDATE
+$set_art = "dtMajArt = '$dtMajArt',
+libTitrArt = '$libTitrArt',
+libChapoArt = '$libTitrArt', 
+libAccrochArt = '$libAccrochArt',
+parag1Art = '$parag1Art', 
+libSsTitr1Art = '$libSsTitr1Art',
+parag3Art = '$parag3Art',
+libConclArt = '$libConclArt', 
+urlPhotArt = '$nom_image', 
+numThem = '$numThem'";
+$where_num = "numArt = '$numArt'";
+$table_art = "ARTICLE";
 
 // UPDATE INFOS GENERALES
-sql_update($table_art, $set_art, $where_num);
 sql_delete('MOTCLEARTICLE', $where_num);
+sql_update($table_art, $set_art, $where_num);
 
 foreach ($numMotCle as $mot){
-    sql_insert('MOTCLEARTICLE', 'numArt, numMotCle', "$lastArt, $mot");
+    sql_insert('MOTCLEARTICLE', 'numArt, numMotCle', "$numArt, $mot");
 }
 
 header('Location: ../../views/backend/articles/list.php');
